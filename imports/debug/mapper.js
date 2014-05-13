16a1f7b8e79c7a0fe4f3483a941c3cd77b949634
Peter Johnson <wiz@wiz.co.nz>
2014-05-13 18:34:00 +0700

    update debug mapper

--- imports/debug/mapper.js
+++ imports/debug/mapper.js
@@ -2,10 +2,24 @@
 var Transform = require('stream').Transform,
     mapper = new Transform({ objectMode: true });
 
+// Record property stats
+var stats = { total: 0 };
+
+setInterval( function(){
+  console.log( 'stats', stats );
+});
+
 mapper._write = function( data, enc, next ){
 
-  console.log( JSON.stringify( data, null, 2 ) );
+  stats.total++;
+  for( var attr in data.properties ){
+    if( !stats[attr] ) stats[attr] = 0;
+    if( data.properties[attr] ) stats[attr]++;
+  }
 
+  // console.log( JSON.stringify( data, null, 2 ) );
+
+  next();
 }
 
 module.exports = mapper;
