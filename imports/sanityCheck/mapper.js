c6c70b50cc871de081c1d8e2b67ff80d71ab1f71
Peter Johnson <wiz@wiz.co.nz>
2014-05-16 20:54:47 +0700

    fix stats

--- imports/sanityCheck/mapper.js
+++ imports/sanityCheck/mapper.js
@@ -1,34 +1,39 @@
 
-var Transform = require('stream').Transform,
-    mapper = new Transform({ objectMode: true });
+var Transform = require('stream').Transform;
 
-mapper._write = function( data, enc, next ){
+module.exports = function(){
 
-  // Skip invalid records
-  if( !data ){
-    // console.error( 'SANITY CHECK - INVALID DATA' );
-    this.emit( 'invalid', data );
-  }
+    var mapper = new Transform({ objectMode: true });
 
-  // Skip invalid records
-  else if( !data.geometry || !data.geometry.coordinates || !data.geometry.coordinates.length ){
-    // console.error( 'SANITY CHECK - INVALID GEOMETRY', data.geometry );
-    this.emit( 'invalid', data );
-  }
+    mapper._write = function( data, enc, next ){
 
-  // Skip records where geometry is too large for ES to process
-  else if( data.geometry.coordinates.length > 1000 ){
-    // console.error( 'SANITY CHECK - COORDS MAX LENGTH', data.geometry.coordinates.length );
-    this.emit( 'invalid', data );
-  }
+      // Skip invalid records
+      if( !data ){
+        // console.error( 'SANITY CHECK - INVALID DATA' );
+        this.emit( 'invalid', data );
+      }
 
-  // Record is OK
-  else {
-    this.push( data );
-    this.emit( 'ok' );
-  }
+      // Skip invalid records
+      else if( !data.geometry || !data.geometry.coordinates || !data.geometry.coordinates.length ){
+        // console.error( 'SANITY CHECK - INVALID GEOMETRY', data.geometry );
+        this.emit( 'invalid', data );
+      }
 
-  next();
+      // Skip records where geometry is too large for ES to process
+      else if( data.geometry.coordinates.length > 1000 ){
+        // console.error( 'SANITY CHECK - COORDS MAX LENGTH', data.geometry.coordinates.length );
+        this.emit( 'invalid', data );
+      }
+
+      // Record is OK
+      else {
+        this.push( data );
+        this.emit( 'ok' );
+      }
+
+      next();
+    }
+
+    return mapper;
+
 }
-
-module.exports = mapper;
